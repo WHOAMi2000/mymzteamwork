4bdc4acb655baf70c658d024b45ec7cd4cc5c111
WHOAMi2000 <71266778+WHOAMi2000@users.noreply.github.com>
2020-12-11 01:09:02 +0800

    Fix bug

--- LINEsufe/miniprogram/pages/questionary/questionary.js
+++ LINEsufe/miniprogram/pages/questionary/questionary.js
@@ -24,11 +24,7 @@ Page({
   },
    
   nextq: function () {
-    if (this.data.id < 19) {
-      this.setData({
-        id: this.data.id + 1,
-      })
-    }
+    
   },
    
   lastq: function (e) {
@@ -43,71 +39,79 @@ Page({
     console.log(e.detail.value);
     var a = e.detail.value.answer;
     var id = this.data.id;
+    console.log(id);
     ans[id] = a;
     this.setData({
       answer: ans,
     })
     console.log(this.data.answer);
-   
+    if (this.data.id < 19) {
+      this.setData({
+        id: this.data.id + 1,
+      })
+    }
+    if(id == 19){
+      var finish;
+      var i = 0;
+      var _this = this;
+      while (i < 20) {
+        if (ans[i] == "") {
+          finish = 'false';
+          break;
+        } else {
+          finish = 'true';
+        }
+        i++;
+      }
+      if (finish == 'false') {
+        wx.showModal({
+          title: '无法提交',
+          content: '您还有部分题目未完成，请检查后重新提交',
+          showCancel: false,
+          confirmColor: '#fcbe39',
+          confirmText: "好的",
+          success(res) {
+            _this.setData({
+              id: i,
+            })
+          }
+        })
+      } else {
+        var score = 5;
+        var stdans = ["C","B","B","C","B","A","D","A","C","B","D","D","A","B","D","A","C","B","A"]
+        for(var i = 0; i < 19; i++){
+          if(stdans[i] == ans[i]){
+            score = score + 5;
+          }
+        }
+        if(score >= 80){
+          wx.showModal({
+            title: '答题结果',
+            content: '您总共获得了'+score+'分',
+            showCancel: false,
+            confirmColor: '#fcbe39',
+            confirmText: "好的",
+          })
+          //nevigate
+          wx.redirectTo({
+            url: '../../pages/register/register',
+          })
+        }else{
+          wx.showModal({
+            title: '答题结果',
+            content: '您总共获得了'+score+'分，没有达到80分，再接再厉！',
+            showCancel: false,
+            confirmColor: '#fcbe39',
+            confirmText: "好的",
+          })
+        }
+      }
+    }
   },
    
   //判断答题完成情况
   formSubmit: function() {
-    var finish;
-    var i = 0;
-    var _this = this;
-    while (i < 20) {
-      if (ans[i] == "") {
-        finish = 'false';
-        break;
-      } else {
-        finish = 'true';
-      }
-      i++;
-    }
-    if (finish == 'false') {
-      wx.showModal({
-        title: '无法提交',
-        content: '您还有部分题目未完成，请检查后重新提交',
-        showCancel: false,
-        confirmColor: '#fcbe39',
-        confirmText: "好的",
-        success(res) {
-          _this.setData({
-            id: i,
-          })
-        }
-      })
-    } else {
-      var score = 5;
-      var stdans = ["C","B","B","C","B","A","D","A","C","B","D","D","A","B","D","A","C","B","A"]
-      for(var i = 0; i < 19; i++){
-        if(stdans[i] == ans[i]){
-          score = score + 5;
-        }
-      }
-      if(score >= 80){
-        wx.showModal({
-          title: '答题结果',
-          content: '您总共获得了'+score+'分',
-          showCancel: false,
-          confirmColor: '#fcbe39',
-          confirmText: "好的",
-        })
-        //nevigate
-        wx.redirectTo({
-          url: '../../pages/register/register',
-        })
-      }else{
-        wx.showModal({
-          title: '答题结果',
-          content: '您总共获得了'+score+'分，再接再厉！',
-          showCancel: false,
-          confirmColor: '#fcbe39',
-          confirmText: "好的",
-        })
-      }
-    }
+    
   },
 
   /**
